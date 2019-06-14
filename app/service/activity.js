fd1c02a865180248b5bd078141070d750aec607d
张爱飞 <zhangaifei@u51.com>
2019-06-14 11:41:23 +0800

    1 修改发布状态接口

--- app/service/activity.js
+++ app/service/activity.js
@@ -26,65 +26,79 @@ module.exports = app => {
   class activity extends app.Service {
     // CRUD for Template
     async findAll({ conditions = {} } = {}) {
-      const query = await app.model.Activity.find(conditions)
-        .catch(err => {
-          app.logger.error(DBLOG_PREFIX, err);
-          throw err;
-        });
+      const query = await app.model.Activity.find(conditions).catch(err => {
+        app.logger.error(DBLOG_PREFIX, err);
+        throw err;
+      });
       return query;
     }
     async findOne({ conditions = {} } = {}) {
-      const checkResult = existCheck(conditions, {
-        id: 'should not be undefined.',
-      }, 'findOne');
+      const checkResult = existCheck(
+        conditions,
+        {
+          id: "should not be undefined."
+        },
+        "findOne"
+      );
       if (checkResult instanceof Error) {
         throw new Error(`${DBLOG_PREFIX} ${checkResult.toString()}`);
       }
 
-      const query = await app.model.Activity.findOne(conditions)
-        .catch(err => {
+      const query = await app.model.Activity.findOne(conditions).catch(
+        err => {
           app.logger.error(DBLOG_PREFIX, err);
           throw err;
-        });
+        }
+      );
       return query;
     }
     async create({ payload = {} }) {
       const newActivity = new app.model.Activity(payload);
-      const query = await newActivity.save()
-        .catch(err => {
-          app.logger.error(DBLOG_PREFIX, err);
-          throw err;
-        });
+      const query = await newActivity.save().catch(err => {
+        app.logger.error(DBLOG_PREFIX, err);
+        throw err;
+      });
       return query;
     }
-    async udpate({ conditions = {}, payload = {} }) {
-      const checkResult = existCheck(conditions, {
-        id: 'should not be undefined.',
-      }, 'udpate');
+    async update({ conditions = {}, payload = {} }) {
+      const checkResult = existCheck(
+        conditions,
+        {
+          id: "should not be undefined."
+        },
+        "udpate"
+      );
       if (checkResult instanceof Error) {
         throw new Error(`${DBLOG_PREFIX} ${checkResult.toString()}`);
       }
       console.log(payload, "payload");
-      const query = await app.model.Activity.updateOne(conditions, payload)
-        .catch(err => {
-          app.logger.error(DBLOG_PREFIX, err);
-          throw err;
-        });
+      const query = await app.model.Activity.updateOne(
+        conditions,
+        payload
+      ).catch(err => {
+        app.logger.error(DBLOG_PREFIX, err);
+        throw err;
+      });
       return query;
     }
     async deleteOne({ conditions = {} }) {
-      const checkResult = existCheck(conditions, {
-        id: 'should not be undefined.',
-      }, 'deleteOne');
+      const checkResult = existCheck(
+        conditions,
+        {
+          id: "should not be undefined."
+        },
+        "deleteOne"
+      );
       if (checkResult instanceof Error) {
         throw new Error(`${DBLOG_PREFIX} ${checkResult.toString()}`);
       }
 
-      const query = await app.model.Activity.deleteOne(conditions)
-        .catch(err => {
+      const query = await app.model.Activity.deleteOne(conditions).catch(
+        err => {
           app.logger.error(DBLOG_PREFIX, err);
           throw err;
-        });
+        }
+      );
       return query;
     }
   }
